04b98615a173d521c150cdca4d0333abed53acfb
PavithraReddy30 <pavithra409409@gmail.com>
2026-06-14 16:08:44 +0530

    second message

--- first_code.py
+++ first_code.py
@@ -1,3 +1,49 @@
 for i in range(10):
     print(i)
 print("Done!")
+
+
+
+# Write a function that calculates the below: ( Mathematics - Geometry) 
+# Area of the Circle
+# Circumference of the Circle
+# Volume and Surface area of a Cylinder
+# Area of the rectangle
+# Perimeter of the rectangle
+# Area of the Square
+
+pie = 3.141
+radius = 2
+side_of_the_sqaure = 4
+length = 4
+width = 8
+height = 4
+
+area_of_the_circle = pie * (radius ** 2)
+
+circumference_of_the_circle = 2*pie*radius
+
+volume_of_the_cylinder = pie*radius*height
+
+surface_of_the_cylinder = 2*pie*radius*(radius+height)
+
+area_of_the_sqaure = (side_of_the_sqaure)*2
+
+perimeter_of_the_rectangle = 2*(length*width)
+
+area_of_the_rectangle = length*width
+
+print(area_of_the_circle)
+
+print(circumference_of_the_circle)
+
+print(volume_of_the_cylinder)
+
+print(surface_of_the_cylinder)
+
+print(area_of_the_sqaure)
+
+print(perimeter_of_the_rectangle)
+
+print(area_of_the_rectangle)
+
